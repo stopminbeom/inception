756a9e04cac19fc16d0be052c1c291bd2fee1d1b
stopminbeom <mseo@student.42seoul.kr>
2022-06-02 01:56:24 +0900

    Fix: mariadb 관련 에러 수정

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -47,3 +47,5 @@ fi
 # https://wiki.alpinelinux.org/wiki/MariaDB
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*skip-networking.*|skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld --user=mysql --console
