8e2d7f16a25022b5bb528c324fcb9f54b6c08ce1
Minbeom Seo <mseo@c2r5s8.42seoul.kr>
2022-06-18 20:38:43 +0900

    Fix : theme out

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -12,9 +12,7 @@ if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	wp core download && \
 	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8mb4" && \
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email && \
-	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD && \
-	wp theme install blocksy --activate && \
-	wp plugin update --all'
+	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD'
 fi
 
 exec php-fpm7 -F
